14af434d2692cc1357ffdcacb55cb22ea29d54c5
Fernando Paredes Rios <fprios112@gmail.com>
2025-02-18 21:43:54 -0500

    updating images

--- utils/mockData.ts
+++ utils/mockData.ts
@@ -4,55 +4,45 @@ export const usersAmistad = [
     name: "Sandra Gómez",
     age: 21,
     image:
-      "../assets/images/testphoto1.png",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto1.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 2,
     name: "Nami Ayanami",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto2.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 3,
     name: "Linda Carter",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto3.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 4,
     name: "Sakura Haruno",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto1.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 5,
     name: "Tsunade Senju",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto2.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
 ];
 
@@ -62,55 +52,45 @@ export const usersCitas = [
     name: "Bulma",
     age: 22,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto3.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 2,
     name: "Misty",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto1.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 3,
     name: "Rey Ayanami",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto2.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 4,
     name: "Sakura Card Captors",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto3.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 5,
     name: "Jeniffer Lawrence",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto1.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
 ];
 
@@ -120,81 +100,71 @@ export const usersRelacion = [
     name: "Carmen Electra",
     age: 22,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto2.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 2,
     name: "Petra Parker",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto3.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 3,
     name: "Vilma Picapiedra",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto1.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 4,
     name: "Betty Marmol",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto2.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
   {
     id: 5,
     name: "Alicia Delgado",
     age: 21,
     image:
-      "",
+      "https://github.com/ferparrios/fulltimeforce-test/blob/main/assets/images/testphoto3.png?raw=true",
     location: "Surco, Perú",
-    intereses: [
-      'Viajar', 'Leer', 'Amistad', 'Salir de Fiesta', 'Pasarlo bien'
-    ]
+    intereses: ["Viajar", "Leer", "Amistad", "Salir de Fiesta", "Pasarlo bien"],
   },
 ];
 
 export const routes = [
   {
-    route: 'LeCafé',
-    icon: '../../assets/images/drawer-icon-1.svg',
+    route: "LeCafé",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-1.svg",
   },
   {
-    route: 'Mensajes',
-    icon: '../../assets/images/drawer-icon-2.svg',
+    route: "Mensajes",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-2.svg",
   },
   {
-    route: 'Matches',
-    icon: '../../assets/images/drawer-icon-3.svg',
+    route: "Matches",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-3.svg",
   },
   {
-    route: 'Mi Perfil',
-    icon: '../../assets/images/drawer-icon-4.svg',
+    route: "Mi Perfil",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-4.svg",
   },
   {
-    route: 'Tutorial',
-    icon: '../../assets/images/drawer-icon-5.svg',
+    route: "Tutorial",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-5.svg",
   },
   {
-    route: 'Ajustes',
-    icon: '../../assets/images/drawer-icon-6.svg',
+    route: "Ajustes",
+    icon: "https://raw.githubusercontent.com/ferparrios/fulltimeforce-test/ef23cc8eb8c1c86cb1e3c4dd774444672b4408bc/assets/images/drawer-icon-6.svg",
   },
 ];
